afa466160aa9d7c04bdb78405df0b36ca59cc693
William Weckl <william.weckl@gmail.com>
2018-01-07 21:12:45 -0200

    upload progress

--- app/components/file-uploader.js
+++ app/components/file-uploader.js
@@ -62,31 +62,75 @@ export default Component.extend({
     let file = fileInput.files[0];
     let reader = new FileReader();
 
+    _self.set('loadingProgress', 0);
+
     console.log("esta uploadiando 1");
 
+    console.log("reader", reader);
+
     reader.addEventListener("load", function(){
+      console.log("event load");
+
       let startRange = _self.startRange();
       let endRange = _self.endRange();
 
       let payload = {
         binary: reader.result.split(",", 2)[1],
         filename: file.name,
-        start_range: startRange,
-        end_range: endRange
+        start_range: parseInt(startRange),
+        end_range: parseInt(endRange)
       };
 
-      console.log("esta uploadiando 2");
+      // console.log("esta uploadiando 2", _self.get('socket'));
+      // console.log("esta uploadiando 2", _self.get('socket.socket'));
+      // console.log("esta uploadiando 2", _self.get('socket.socket.conn'));
+      console.log("esta uploadiando 2", _self.get('socket.socket.conn.bufferedAmount'));
 
-      const push = channel.push("upload:file", payload);
+      const push = channel.push("upload:file", payload, 9999999999);
+      _self.computeLoadingProgress(file);
       console.log(push);
-      console.log(channel.pushBuffer);
-      console.log(push.pushBuffer);
 
-      push.receive(
-        "ok", (reply) => {
-          console.log("got reply", reply)
-      })
+      // console.log("esta uploadiando 3", _self.get('socket'));
+      // console.log("esta uploadiando 3", _self.get('socket.socket'));
+      // console.log("esta uploadiando 3", _self.get('socket.socket.conn'));
+      console.log("esta uploadiando 3", _self.get('socket.socket.conn.bufferedAmount'));
+
+      push
+        .receive("ok", function (msg) {
+          console.log("created message", msg);
+          console.log("bufferedAmount", _self.get('socket.socket.conn.bufferedAmount'));
+          _self.set('processingFile', false);
+        })
+        .receive("error", (reasons) => console.log("create failed", reasons) )
+        .receive("timeout", () => console.log("Networking issue..."))
     }, false)
     reader.readAsDataURL(file)
   },
+
+  computeLoadingProgress: function (file) {
+    console.log('computeLoadingProgress');
+
+    const _self = this;
+    let bufferedAmount = _self.get('socket.socket.conn.bufferedAmount');
+
+    if (bufferedAmount === 0) {
+      _self.set('loadingProgress', 100);
+      _self.set('processingFile', true);
+      return;
+    }
+
+    console.log('file', file);
+    console.log('bufferedAmount', bufferedAmount);
+
+    var loaded = file.size - bufferedAmount;
+    var percentage = Math.round((loaded * 100) / file.size );
+    _self.set('loadingProgress', percentage);
+
+    // Wait 1 second and fetch percentage again
+    return setTimeout(function() {
+      console.log('computeLoadingProgress setInterval');
+      return _self.computeLoadingProgress(file);
+    }, 1000);
+  },
+
 });
